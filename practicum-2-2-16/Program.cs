267400bd0a2b534f73663188ba237aa45ae09da6
Kudyakov Artem <t3m8ch@yandex.ru>
2024-09-20 09:39:02 +0400

    homework

---
+++ practicum-2-2-16/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите коэффициент a: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.Write("Введите коэффициент b: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.Write("Введите коэффициент c: ");
+double c = double.Parse(Console.ReadLine());
+
+string equation_text = string.Format("{0}x^2 + {1}x + {2}", a, b, c);
+Console.WriteLine((b * b - 4 * a * c) >= 0 ? equation_text + " имеет решение" : equation_text + " не имеет решения");
---
+++ practicum-3-2-2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите значение x: ");
+double x = double.Parse(Console.ReadLine());
+
+Console.Write("Введите значение y: ");
+double y = double.Parse(Console.ReadLine());
+
+double circle = x * x + y * y;
+
+if (circle == 9 || circle == 49) {
+  Console.WriteLine("На границе");
+} else if (circle < 49 && circle > 9) {
+  Console.WriteLine("Да");
+} else {
+  Console.WriteLine("Нет");
+}
