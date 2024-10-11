6fb2f46a8722215397cb6cc3121a514bdd565e41
Artem Kudyakov <t3m8ch@yandex.ru>
2024-10-11 07:48:57 +0400

    Practicum 5-4-4

---
+++ practicum-5-4-4/Program.cs
@@ -0,0 +1,15 @@
+﻿void printNums(uint curr, uint n) {
+    for (uint i = 0; i < curr; i++) {
+        Console.Write("{0}\t", curr);
+    }
+
+    Console.WriteLine();
+
+    if (curr != n) {
+        printNums(curr + 1, n);
+    }
+}
+
+Console.Write("n = ");
+uint n = uint.Parse(Console.ReadLine());
+printNums(1, n);
