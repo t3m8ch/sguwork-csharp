db1715b53122cd89428e165eaed16f92087a6b7c
Artem Kudyakov <t3m8ch@yandex.ru>
2024-10-11 07:37:38 +0400

    practicum 5-3-4

---
+++ practicum-5-3-4/Program.cs
@@ -0,0 +1,19 @@
+﻿uint greatestCommonDivisor(uint a, uint b) {
+    if (a == b) {
+        return a;
+    }
+
+    if (a > b) {
+        return greatestCommonDivisor(a - b, b);
+    }
+
+    return greatestCommonDivisor(a, b - a);
+}
+
+Console.Write("a = ");
+uint a = uint.Parse(Console.ReadLine());
+
+Console.Write("b = ");
+uint b = uint.Parse(Console.ReadLine());
+
+Console.WriteLine("НОД = {0}", greatestCommonDivisor(a, b));
