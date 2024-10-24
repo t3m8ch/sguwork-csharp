98a03b0c539680b47e944916f061a7b87141f878
Kudyakov Artem <t3m8ch@yandex.ru>
2024-10-24 15:04:32 +0400

    5-6-4

---
+++ practicum-5-6-4/Program.cs
@@ -0,0 +1,15 @@
+﻿void f(uint start, uint n) {
+  for (uint i = 1; i <= start; i++) {
+    Console.Write("{0}\t", i);
+  }
+  Console.WriteLine();
+
+  if (start < n) {
+    f(start + 1, n);
+  }
+}
+
+Console.Write("n = ");
+uint n = uint.Parse(Console.ReadLine());
+
+f(1, n);
